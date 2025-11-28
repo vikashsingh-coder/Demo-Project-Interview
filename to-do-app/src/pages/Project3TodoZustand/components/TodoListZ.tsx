b88e10bd6bb93f117ff01bba827eeb41e6bb938e
mentorvikash <mentorvikash@gmail.com>
2025-11-28 23:11:22 +0530

    add todo list

---
+++ to-do-app/src/pages/Project3TodoZustand/components/TodoListZ.tsx
@@ -0,0 +1,21 @@
+import { useTodoStore } from "../../../store/todo";
+
+export function TodoListZ() {
+  const { todos, toggleTodo } = useTodoStore();
+  return (
+    <div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            <span>{todo.title}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- to-do-app/src/pages/Project3TodoZustand/index.tsx
+++ to-do-app/src/pages/Project3TodoZustand/index.tsx
@@ -1,10 +1,12 @@
 import { TodoZ } from "./components/TodoZ";
+import { TodoListZ } from "./components/TodoListZ";
 
 function Main() {
   return (
     <div>
       <h1>Zustand Todo</h1>
       <TodoZ />
+      <TodoListZ />
     </div>
   );
 }
